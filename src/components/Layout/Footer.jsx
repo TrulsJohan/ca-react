5e33ea4214305f65a85099c5ed4a4d24907f8145
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2025-03-05 16:37:17 +0100

    chore: header footer test

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,8 +1,7 @@
-// components/Footer.js
 export function Footer() {
     return (
         <footer className="bg-stone-400 text-gray-900 py-6 mt-8 font-sans">
-            <div className="container mx-auto flex flex-col items-center justify-center gap-4 sm:flex-row sm:justify-between sm:gap-0">
+            <div className="container mx-auto flex flex-col items-center justify-center">
                 <p className="text-sm sm:text-base">
                     © {new Date().getFullYear()} All rights
                     reserved.
